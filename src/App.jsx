02fa5da25b14e8a01150f264c91ef9e308e9a559
Karan Shah <karanshah0206@outlook.com>
2022-02-03 12:57:41 +0530

    Add Header + Background Gradient

--- src/App.jsx
+++ src/App.jsx
@@ -14,13 +14,23 @@ class App extends Component {
     fetch("https://jsonplaceholder.typicode.com/users").then(response => response.json()).then(users => this.setState({monsters: users}));
   }
 
-  render() {
+  /**
+   * JavaScript doesn't allow functions to automatically get context of their class.
+   * As a result, they must be manually bound in the constructor using the following syntax:
+   * this.searchMonsters = this.searchMonsters.bind(this);
+   * However, ES6 allows us to use arrow functions, so that this is not required.
+   * If using regular functions, always remember to bind that function in the constructor to access this.
+   * Note: Only applies for functions not inherited via Component (e.g. won't apply to  render()).
+  */
+  searchMonsters = e => { this.setState({searchQuery: e.target.value}); }
 
+  render() {
     let filteredMonsters = this.state.monsters.filter(m => m.name.toLowerCase().includes(this.state.searchQuery.toLowerCase()));
 
     return (
       <div className="App">
-        <SearchBox placeholder={"Search Monsters..."} handleChange={e => this.setState({searchQuery: e.target.value})} />
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder={"Search Monsters..."} handleChange={this.searchMonsters} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
